23530c7edffa3309cd1c0b20e3b0898d285d447c
Fluent Immersion <158618477+teachermarcelo@users.noreply.github.com>
2026-04-21 05:48:55 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,6 @@
 window.SUPABASE_CONFIG = {
+    // ✅ Deve ser EXATAMENTE esta URL do seu projeto
     url: 'https://rvgcniaowzmsudzliozf.supabase.co',
+    // ✅ Deve ser a chave 'anon public' (não a service_role)
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ2Z2NuaWFvd3ptc3Vkemxpb3pmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4MjQxNzQsImV4cCI6MjA5MTQwMDE3NH0.uwwKFLuK-XyPoXPrB6_CseRTiD9d-iyMQSPWrFw-l-I'
 };
